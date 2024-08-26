6055a12e9588a091f982bea9fb4f496ffcdd9b4e
ERIC LIONEL MARASCH DONZIS <51398893@ort>
2024-08-26 08:28:00 -0300

    Creación del Proyecto

---
+++ wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿document.querySelector('.roulette').addEventListener('click', function() {
+    const roulette = this;
+    const degrees = Math.floor(Math.random() * 360) + 360 * 5; 
+    roulette.style.transition = 'transform 5s ease-out';
+    roulette.style.transform = `rotate(${degrees}deg)`;
+
+ 
+    setTimeout(() => {
+        roulette.style.transition = 'none';
+        roulette.style.transform = `rotate(${degrees % 360}deg)`; 
+    }, 5000);
+});
+
